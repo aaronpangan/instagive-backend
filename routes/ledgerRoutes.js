dea5fed051e11746280501a3fe4eebcccb59035b
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-04-18 21:29:06 +0800

    Update ledgerRoutes.js

--- routes/ledgerRoutes.js
+++ routes/ledgerRoutes.js
@@ -14,7 +14,7 @@ const verifyToken = require('../middleware/verifyToken');
 router.post('/getall', [verifyToken], allRecord);
 
 
-router.post('/:postId', [verifyToken], ((req, res) => {console.log(hello)}));
+router.post('/:postId', [verifyToken], ((req, res) => {console.log('hello')}));
 
 router.post('/pending/:postId', [verifyToken], donateButton);
 
